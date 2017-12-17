6352fa25b03509b7233fdda48af011cd8e13a8ca
Kevin Kulp <musicthreads@hotmail.com>
2017-12-17 17:21:51 -0500

    It looks like the issue has nothing to do with the AuthService. I added Router to home component and I'm getting the same error. I removed referenced to AuthService.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import {Component} from '@angular/core';
-import { AuthService } from './auth/auth.service';
+//import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app',
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  constructor(public auth: AuthService) {}
+  //constructor(public auth: AuthService) {}
 }
